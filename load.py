d4f962c1d9e47c277c5de55f74de99164cfc49b0
Juan Valentin <juan@verdi>
2019-06-10 11:17:44 +0200

    URL list extracted

--- load.py
+++ load.py
@@ -7,16 +7,15 @@ input_filename = open(FILENAME, "r")
 bookmark_data = json.load(input_filename)
 input_filename.close()
 
+# Compute number of elements, including categories and end nodes
 elements = len(bookmark_data)
 print(elements,"entries in bookmark_data") 
 #print(json.dumps(bookmark_data, sort_keys=True, indent=4))
 
-for i in bookmark_data:
-    print('#',i["id"])
-    try:
-        url = i["url"]
-    except NameError:
-        print("No url")
+for dict in bookmark_data:
+    print('#',dict["id"], end="")
+    if "url" in dict:
+        print(' ->',dict["url"])
     else:
-        print(url)
+        print(':',dict["title"])
 
